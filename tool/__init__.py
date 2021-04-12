1afdb52eb24fc2ee9d0cc4e73a44a8a8b2528ce9
Laura Rodríguez Navas <lrodrin@users.noreply.github.com>
2021-04-12 09:26:05 +0200

    Update __init__.py

--- tool/__init__.py
+++ tool/__init__.py
@@ -16,6 +16,6 @@
 # limitations under the License.
 
 __author__ = 'Laura Rodriguez-Navas <https://orcid.org/0000-0003-4929-1219> and Laia Codó <https://orcid.org/0000-0002-6797-8746>'
-__copyright__ = '© 2020 Barcelona Supercomputing Center (BSC), ES'
+__copyright__ = '© 2020-2021 Barcelona Supercomputing Center (BSC), ES'
 __license__ = 'Apache 2.0'
 __version__ = '0.0.1'
